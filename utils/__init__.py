eb92ad4b3c53544964a83e0b9451e6a06766da5a
af-Athameem <athameem@ampliforce.com>
2025-03-03 11:47:53 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -15,16 +15,9 @@ from utils.sharepoint import (
     get_file_item,
     upload_to_eval_benchmark,
     get_access_token,
-    get_site_id
-)
-
-# Database functions 
-from utils.sharepoint import (
-    load_json_db_file,
-    save_json_db_file,
-    get_all_tags,
-    get_all_documents,
-    add_question
+    get_site_id,
+    get_all_tags_from_list,
+    get_all_documents_from_list
 )
 
 # UI helper functions
@@ -34,6 +27,7 @@ from utils.form import (
     handle_new_tag
 )
 
+# S3 functions
 from utils.s3 import (
     upload_file, 
     list_files, 
@@ -41,6 +35,7 @@ from utils.s3 import (
     read_json_from_s3,
     write_json_to_s3
 )
+
 # Define which symbols should be exported with "from utils import *"
 __all__ = [
     # Auth functions
@@ -59,19 +54,16 @@ __all__ = [
     'get_access_token',
     'get_site_id',
     
-    # Database functions
-    'load_json_db_file',
-    'save_json_db_file',
-    'get_all_tags',
-    'get_all_documents',
-    'add_question',
+    # List-based functions (previously DB functions)
+    'get_all_tags_from_list',
+    'get_all_documents_from_list',
     
     # UI helper functions
     'add_document',
     'remove_document',
-    'handle_new_tag'
+    'handle_new_tag',
     
-    #s3
+    # S3 functions
     'upload_file', 
     'list_files', 
     'file_exists',
